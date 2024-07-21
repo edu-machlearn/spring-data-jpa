736d028a503f14d345305270102bdc80b2ef3220
Biswaranjan Mohapatra <bmohapatra1983@gmail.com>
2024-07-21 20:37:27 +0530

    Remove Test Commit

--- demo-springData-jpa/src/main/java/com/biswa/DemoSpringDataJpaApplication.java
+++ demo-springData-jpa/src/main/java/com/biswa/DemoSpringDataJpaApplication.java
@@ -9,7 +9,6 @@ public class DemoSpringDataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringDataJpaApplication.class, args);
 		System.out.println("Main Application in Running on Port : 2024");
-		System.out.println("Git Test Commit..");
 	}
 
 }

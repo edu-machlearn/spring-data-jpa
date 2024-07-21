50c12b3ad27cec036ba46f5e7f033beadd5d7302
Biswaranjan Mohapatra <bmohapatra1983@gmail.com>
2024-07-21 20:30:05 +0530

    Test Git

--- demo-springData-jpa/src/main/java/com/biswa/DemoSpringDataJpaApplication.java
+++ demo-springData-jpa/src/main/java/com/biswa/DemoSpringDataJpaApplication.java
@@ -9,6 +9,7 @@ public class DemoSpringDataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringDataJpaApplication.class, args);
 		System.out.println("Main Application in Running on Port : 2024");
+		System.out.println("Git Test Commit..");
 	}
 
 }
